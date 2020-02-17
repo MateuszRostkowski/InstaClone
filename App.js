cd58b0819d0e638f615edb95bc3b28899b28a71a
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2020-02-17 21:00:43 +0100

    Add Card to App.js

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import AuthorRow from './components/AuthorRow';
+import Card from './components/Card';
 
 const Container = styled.SafeAreaView``;
 
 const App = () => {
   return (
     <Container>
-      <AuthorRow
-        fullName={'First Last'}
+      <Card
+        fullname={'First Last'}
         linkText={'Comments'}
         onPressLinkText={() => {
           console.log('Pressed link!');
         }}
+        image={{uri: 'https://unsplash.it/600/600'}}
       />
     </Container>
   );
